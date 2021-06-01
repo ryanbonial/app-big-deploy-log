5a75e661fe93d2a5786eda2055a7d61a327273bd
Ryan Bonial <ryanbonial@netlify.com>
2021-06-01 17:51:53 -0600

    more...?

--- print-a-lot.js
+++ print-a-lot.js
@@ -1,5 +1,5 @@
 const lorem = 'Lorem ipsum dolor sit amet, consectetur adipiscing elit, sed do eiusmod tempor incididunt ut labore et dolore magna aliqua. Ut enim ad minim veniam, quis nostrud exercitation ullamco laboris nisi ut aliquip ex ea commodo consequat. Duis aute irure dolor in reprehenderit in voluptate velit esse cillum dolore eu fugiat nulla pariatur. Excepteur sint occaecat cupidatat non proident, sunt in culpa qui officia deserunt mollit anim id est laborum.'
 
-for (let index = 0; index < 1000; index++) {
+for (let index = 0; index < 2000; index++) {
   console.log(`${index} ${lorem}`)
 }
